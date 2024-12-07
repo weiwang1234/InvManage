6fe92065be1ce0b91acd239632c78c08c1ad1377
ww <106657674+weiwang1234@users.noreply.github.com>
2024-12-07 22:30:24 +0800

    项目搭建完成

---
+++ src/main/java/com/happycode/InvManageApplication.java
@@ -0,0 +1,12 @@
+package com.happycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvManageApplication.class, args);
+    }
+}
